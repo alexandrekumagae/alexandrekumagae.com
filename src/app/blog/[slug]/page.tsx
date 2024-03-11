cf65c37c711b9bace8babd4e70b6845d96048b37
Alexandre Kumagae <alexandre@movedigital.com.br>
2024-03-11 13:55:07 -0300

    fix: bug fix on structure of post item

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,23 +1,29 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Título de Post',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum perspiciatis animi perferendis repellendus mollitia sunt unde quis numquam.',
+}
+
 export default function BlogPost() {
   return (
     <>
       <h1 className="mb-8 text-4xl font-black">Título de post</h1>
-      <main className="mx-auto min-h-[80vh] max-w-2xl max-sm:px-6">
-        <div className="mb-6 flex justify-between">
-          <time className="text-[rgba(113,128,150)]">Fri Mar 19 2021</time>
-          <a href="/blog" className="text-[rgba(113,128,150)] underline">
-            ← Voltar para blog
-          </a>
-        </div>
-        <div>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nam
-            repellendus ad tempora laboriosam iure atque doloremque? Quos quod
-            at quasi soluta voluptatibus laudantium cumque! Eveniet, quibusdam!
-            Natus, ullam tempora.
-          </p>
-        </div>
-      </main>
+      <div className="mb-6 flex justify-between">
+        <time className="text-[rgba(113,128,150)]">Fri Mar 19 2021</time>
+        <a href="/blog" className="text-[rgba(113,128,150)] underline">
+          ← Voltar para blog
+        </a>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nam
+          repellendus ad tempora laboriosam iure atque doloremque? Quos quod at
+          quasi soluta voluptatibus laudantium cumque! Eveniet, quibusdam!
+          Natus, ullam tempora.
+        </p>
+      </div>
     </>
   )
 }
